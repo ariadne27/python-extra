41a6a2164f77dcec4638a5bf0f6253c82acbc29e
Madeline Starr <40212869+ariadne27@users.noreply.github.com>
2019-05-08 16:25:29 -0400

    Adding average score calculation

--- PyWine/main.py
+++ PyWine/main.py
@@ -3,8 +3,10 @@ import csv
 import re
 countries = []
 countriesfl = []
+scorefl = []
+scorerun = 0
+scorerunlist = []
 countriescnt = []
-varieties = []
 increase = 0
 count0 = 0
 csvpath = os.path.join('.', 'resources', 'wine_data8.csv')
@@ -20,20 +22,41 @@ with open(csvpath, newline='') as csvfile:
         #make sure the country element is not black
         if row[1] != "":
             country = row[1]
+            score = row[4]
             #add country to a full list to be counted later
             countriesfl.append(country)
+            scorefl.append(score)
             #if not blank and not currently on the list of countries, add to that list
             if country not in countries:
                 countries.append(country)
-            # variety = row[9]    
-            # if variety not in varieties:
-            #     varieties.append(variety)
-#for each element in counties, count the number of times that element appears on the full list
-#add that number to a count list in the same position as the country it collelates to
+
+#zip the two lists together, then make that zip a list
+scorexcount = zip(countriesfl, scorefl)
+print(type(scorexcount))
+scorexcount = list(scorexcount)
+print(type(scorexcount))
+
 for countrycnt in countries:
+    scorerun = 0
     count = countriesfl.count(countrycnt)
     countriescnt.append(count)
-#for each element in the countries list, print the element and the corresponding count valueS
-for countryf in countries:
-    print(str(countryf) + ": " + str(countriescnt[increase]))
-    increase = increase + 1
+    for lines in scorexcount:
+        if lines[0] != countrycnt:
+            continue
+        else:
+            if lines[1] != "":
+                scorecur = float(lines[1])
+                scorerun = scorerun + scorecur
+    scorerunlist.append(scorerun/count)
+
+roster = zip(countries, countriescnt, scorerunlist)
+
+output_file = os.path.join('.', 'output', 'wine_country.csv')
+
+# open the output file, create a header row, and then write the zipped object to the csv
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(["Country", "Count", "Average Score"])
+
+    writer.writerows(roster)
